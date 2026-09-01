0a9a74f30de8b6b702c5c1b72b099279a6e6d489
dnwls7738 <cjsdnwls7738@gmail.com>
2026-09-01 09:58:25 +0900

    get post

--- board-mvc/src/main/java/kr/ac/kopo/controller/BoardController.java
+++ board-mvc/src/main/java/kr/ac/kopo/controller/BoardController.java
@@ -1,25 +1,29 @@
 package kr.ac.kopo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import kra.ac.kopo.board.vo.BoardVO;
+
+import kr.ac.kopo.board.service.BoardService;
+import kr.ac.kopo.board.vo.BoardVO;
 
 @Controller
 public class BoardController {
 	
-	// 전체 게시글 조회 요청
+	@Autowired
+	private BoardService boardService;
+	
+//	 전체 게시글 조회 요청
 	@RequestMapping("/board")
-	public String list(Object model) {
-		
-		BoardVO board = new BoardVO(1, "제목입니다", "유태웅", "2026-08-26");
-		BoardVO board2 = new BoardVO(1, "제목2입니다", "유탱이", "2026-08-26");
-		BoardVO board3 = new BoardVO(1, "제목3입니다", "유탱이", "2026-08-26");
-		BoardVO board4 = new BoardVO(1, "제목4입니다", "유탱이", "2026-08-26");
-		BoardVO[] boardList = new BoardVO[] {board, board2, board3, board4};
-		
-		model.addAttribute("boardList", boardList);
-		
-		return "board/list";
+	public String list(Model model) throws Exception {
+		List<BoardVO> boardList = boardService.getBoardList();
+		model.addAttribute("boardList",boardList);
+		return "board/list";	
 	}
 	
 	@RequestMapping("/board/detail")
@@ -30,6 +34,16 @@ public class BoardController {
 	@RequestMapping("/board/new")
 	public String newbaord() {
 		return "board/new";
+	}
+	
+	@GetMapping("board/write")
+	public void wirteForm() {
+		System.out.println("get write");
+	}
+	
+	@PostMapping("board/write")
+	public void write() {
+		System.out.println("post write");
 	}
 	
 }
--- board-mvc/src/main/java/kr/ac/kopo/controller/MemberController.java
+++ board-mvc/src/main/java/kr/ac/kopo/controller/MemberController.java
@@ -1,18 +1,29 @@
 package kr.ac.kopo.controller;
 
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.ac.kopo.member.service.MemberService;
+import kr.ac.kopo.member.vo.MemberVO;
 
 @Controller
 public class MemberController {
+	
+	@Autowired
+	private MemberService memberService;
+	
+	
 	@RequestMapping("/members")
-	public String members() {
+	public String members(Model model) throws Exception {
+		List<MemberVO> memberList = memberService.getMemberList();
+		model.addAttribute("memberList", memberList);
 		return "members/members";
 	}
 	
-	@RequestMapping("/members/hong")
-	public String mypage() {
-		return "members/mypage";
-	}
-
 }
